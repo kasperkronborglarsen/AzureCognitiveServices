e7e47b5ffad295bed4ff13b74d852cf71aa852a4
Kasper Kronborg Larsen <kkro@inspari.dk>
2021-02-23 09:01:01 +0100

    Added dependencies

---
+++ AzureConnection/connect.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+import yaml
+
+
+@dataclass(frozen=True)
+class AzureCredentials:
+    """Represents database configuration settings."""
+
+    endpoint: str
+    key: str
+
+    @staticmethod
+    def load(db_path: Path):
+        """Loads configuration from YAML file."""
+        if not db_path.exists():
+            raise Exception(f"Authentication file {db_path} does not exist.")
+
+        with open(db_path, mode="r", encoding="utf-8") as cred:
+            content = yaml.full_load(cred)
+            return DbConfig(
+                endpoint=content.get("endpoint"),
+                key=content.get("key"),
+            )
