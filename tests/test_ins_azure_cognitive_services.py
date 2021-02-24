e3b4565ea9885f3851eb7efe1b1bd40730463d89
Kasper Kronborg Larsen <kkro@inspari.dk>
2021-02-24 15:16:39 +0100

    Added mypy changes to project

--- tests/test_ins_azure_cognitive_services.py
+++ tests/test_ins_azure_cognitive_services.py
@@ -1,5 +1,6 @@
-from ins_azure_cognitive_services import __version__
+from AzureCognitiveServices import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    """Test."""
+    assert __version__ == "0.1.0"
